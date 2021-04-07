4160a11ed4906ff9f1563a560594dbdb29f9d9e7
youichiokuda <youichiokuda@icloud.com>
2021-04-07 21:00:33 +0900

    グラフ追加

--- image.py
+++ image.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
+import seaborn as sns
 
 st.title('ランニング最適化プロジェクト')
 
@@ -7,4 +9,27 @@ df=pd.read_csv('rundiary.csv')
 
 st.table(df)
 
-#st.dataframe(df, 2000, 1000)
+
+st.write(
+    px.bar(df, x='day', y='condition' ,title="コンディション")
+)
+
+st.write(
+    px.bar(df, x='day', y='distance' ,title="距離")
+)
+
+st.write(
+    px.bar(df, x='day', y='span' ,title="スパン")
+)
+
+
+
+st.title('グラフテスト')
+st.line_chart(df)
+
+df_corr = df.corr(method='pearson')
+df_corr
+
+sns.heatmap(df_corr, cmap= sns.color_palette('coolwarm', 10), annot=True,fmt='.2f', vmin = -1, vmax = 1)
+
+
